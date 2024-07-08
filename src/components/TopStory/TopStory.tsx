ab433356f04f3c03bb6a194f72238635dee800e9
Daniel Sierra <danieluchin@gmail.com>
2024-07-08 23:40:57 +0200

    TopStory mui

--- src/components/TopStory/TopStory.tsx
+++ src/components/TopStory/TopStory.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import { ThemeProvider, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import CardMedia from '@mui/material/CardMedia';
+import Paper from '@mui/material/Paper';
+import theme from '../../config/theme';
 import './TopStory.css';
 
 interface TopStoryProps {
@@ -9,32 +13,41 @@ interface TopStoryProps {
   link: string;
 }
 
-const TopStory: React.FC<TopStoryProps> = ({ imageUrl, imageAlt, headline, description, link }) => {
-  console.log(imageUrl);
+const TopStory: React.FC<TopStoryProps> = ({
+  imageUrl,
+  imageAlt,
+  headline,
+  description,
+  link,
+}) => {
   return (
-    <div className="content-card-container" data-cmp-id="top-story">
-      <div className="media-container">
-        <div className="aspect-ratio-container">
-          <div className="aspect-ratio-content">
-            <a className="image-link" href={link} data-testid="image-container">
-              <div className="image-wrapper">
-                <img src={imageUrl} alt={imageAlt} className="img" data-testid="image" />
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="content-container">
-        <div className="headline-container" data-testid="headline-container">
-          <a className="headline-link" href={link} data-testid="link">
-            <h1 className="headline-wrapper">{headline}</h1>
-          </a>
-        </div>
-        <div className="description-container" data-testid="description-container">
-          <p>{description}</p>
-        </div>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Paper data-cmp-id="top-story" style={{ overflowX: 'auto' }}>
+        <a href={link}>
+          <Box className="media-container">
+            <CardMedia
+              component="img"
+              image={imageUrl}
+              alt={imageAlt}
+              className="img"
+              data-testid="image"
+            />
+          </Box>
+          <Box className="content-container">
+            <Typography
+              variant="h4"
+              className="headline-container"
+              fontWeight="bold"
+            >
+              {headline}
+            </Typography>
+            <Typography variant="body1" p={2}>
+              {description}
+            </Typography>
+          </Box>
+        </a>
+      </Paper>
+    </ThemeProvider>
   );
 };
 
